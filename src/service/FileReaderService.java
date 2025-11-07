6f2fcafa1449da440f50ff941eab14ae33f5fa99
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2025-11-07 06:35:59 +0300

    added FileReaderService

---
+++ src/service/FileReaderService.java
@@ -0,0 +1,83 @@
+package service;
+
+import exception.NoMatchingFilesException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
+
+public final class FileReaderService {
+  private FileReaderService(){}
+
+  private static final String PATH_TO_REPORT_FILE = "src\\report.txt";
+  private static final String PATH_TO_SEARCH_DIRECTORY = "src\\input\\";
+
+  public static void launchReadFileService() {
+    try {
+      startProcessingTheFiles(findSuitableFiles((PATH_TO_SEARCH_DIRECTORY)));
+      System.out.println("Парсинг файлов завершен!");
+    } catch (NoMatchingFilesException ex) {
+      System.err.println(ex.getMessage());
+    }
+  }
+
+  public static File[] findSuitableFiles(String path) throws NoMatchingFilesException {
+    File[] files;
+    File folder = new File(path);
+    FilenameFilter filter = (file, name) -> name.endsWith(".txt");
+    String[] suitableFileNames = folder.list(filter);
+
+    assert suitableFileNames != null;
+    if (suitableFileNames.length != 0) {
+      files = new File[suitableFileNames.length];
+      for (int i = 0; i < suitableFileNames.length; i++) {
+        files[i] = new File(path + suitableFileNames[i]);
+      }
+    } else {
+      throw new NoMatchingFilesException();
+    }
+    return files;
+  }
+
+  public static void startProcessingTheFiles(File[] files) {
+    for (File file : files) {
+      new Thread(new MultithreadingFileHandler(file)).start();
+    }
+  }
+
+  public static StringBuilder readTheFile(File file) {
+    StringBuilder textFromTheFile = null;
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      textFromTheFile = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        textFromTheFile.append(line).append("\n");
+      }
+    } catch (IOException ex) {
+      System.err.println(ex.getMessage());
+    }
+    return textFromTheFile;
+  }
+
+  public static void printingReportFileContent() throws NoMatchingFilesException {
+    File reportFile = new File(PATH_TO_REPORT_FILE);
+
+    if (reportFile.exists()) {
+      StringBuilder textFromTheReportFile = readTheFile(reportFile);
+      System.out.println(textFromTheReportFile);
+    } else {
+      throw new NoMatchingFilesException();
+    }
+  }
+
+  public static void launchPrintingReportFileContent() {
+    try {
+      printingReportFileContent();
+    } catch (NoMatchingFilesException ex) {
+      System.err.println(ex.getMessage());
+    }
+  }
+}
