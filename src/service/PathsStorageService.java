498c9bb32ba6158fee050a311ff4ec97593e4241
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2025-11-12 10:35:03 +0300

    added PathStorageService

---
+++ src/service/PathsStorageService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class PathsStorageService {
+  public static final String PATH_TO_INPUT_DIRECTORY = "src\\input\\";
+  public static final String PATH_TO_REPORT_FILE = "src\\report.txt";
+  public static final String PATH_TO_ARCHIVE_DIRECTORY = "src\\archive\\";
+}
