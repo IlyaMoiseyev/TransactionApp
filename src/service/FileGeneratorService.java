5ebcd7e62fe753391e208a9e771c5cfa39ae5b0f
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2025-11-12 10:59:43 +0300

    language has been changed to english, refactored magic numbers into named constants

--- src/service/FileGeneratorService.java
+++ src/service/FileGeneratorService.java
@@ -8,8 +8,16 @@ import java.nio.file.Paths;
 import java.util.Random;
 
 public final class FileGeneratorService {
-  private FileGeneratorService() {}
-
+  private static final int MINIMUM_NUMBERS_OF_ROWS = 1;
+  private static final int MAXIMUM_NUMBERS_OF_ROWS = 11;
+  private static final int MINIMUM_VALUE_OF_ACCOUNT_NUMBER_PART = 10000;
+  private static final int MAXIMUM_VALUE_OF_ACCOUNT_NUMBER_PART = 100000;
+  private static final int MINIMUM_TRANSFER_AMOUNT = 10;
+  private static final int MAXIMUM_TRANSFER_AMOUNT = 10000;
+  private static final int ROUNDING_FACTOR = 100;
+  private static final int MINIMUM_NUMBER_OF_FILES = 2;
+  private static final int MAXIMUM_NUMBER_OF_FILES = 6;
+  private static final String FILE_EXTENSION = ".txt";
   private static final String[] DEFAULT_FILE_NAMES = {
           "accountsAndAmounts", "paymentRegister", "financialTransferSheet",
           "transferAndBalances", "paymentAccountsSummary", "transferAccountsRegistry2025",
@@ -17,11 +25,15 @@ public final class FileGeneratorService {
           "accountsToTransferWithAmount", "accountsWithBalancesForPayment"
   };
 
+  private FileGeneratorService() {}
+
   public static void generateFiles(int numberOfFiles) {
     while (numberOfFiles > 0) {
       StringBuilder fillingText = getFillingForTheFile();
       checkingDirectoryExistence();
-      try (FileWriter fileWriter = new FileWriter("src\\input\\" + getRandomFileName() + ".txt")) {
+      try (FileWriter fileWriter = new FileWriter( PathsStorageService.PATH_TO_INPUT_DIRECTORY
+              + getRandomFileName()
+              + FILE_EXTENSION)) {
         fileWriter.write(String.valueOf(fillingText));
       } catch (IOException ex) {
         System.out.println(ex.getMessage());
@@ -31,7 +43,7 @@ public final class FileGeneratorService {
   }
 
   public static void checkingDirectoryExistence() {
-    Path directory = Paths.get("src", "input");
+    Path directory = Paths.get(PathsStorageService.PATH_TO_INPUT_DIRECTORY);
     if (!Files.exists(directory)) {
       try {
         Files.createDirectories(directory);
@@ -57,7 +69,7 @@ public final class FileGeneratorService {
         counter--;
       }
       if (counter == 0) {
-        System.out.println("Свободные имена файлов исчерпаны!");
+        System.out.println("Unoccupied file names are exhausted!");
       }
     } else {
       DEFAULT_FILE_NAMES[randomIndex] = null;
@@ -67,7 +79,7 @@ public final class FileGeneratorService {
 
   public static StringBuilder getFillingForTheFile() {
     StringBuilder fillingText = new StringBuilder();
-    int numberOfRows = new Random().nextInt(1, 11);
+    int numberOfRows = new Random().nextInt(MINIMUM_NUMBERS_OF_ROWS, MAXIMUM_NUMBERS_OF_ROWS);
 
     for (int i = 0; i < numberOfRows; i++) {
       fillingText.append(getPartOfAccountNumber())
@@ -85,15 +97,15 @@ public final class FileGeneratorService {
   }
 
   public static int getPartOfAccountNumber() {
-    return new Random().nextInt(10000, 100000);
+    return new Random().nextInt(MINIMUM_VALUE_OF_ACCOUNT_NUMBER_PART, MAXIMUM_VALUE_OF_ACCOUNT_NUMBER_PART);
   }
 
   public static double getTransferAmount() {
-    double transferAmount = new Random().nextDouble(10, 10000);
-    return (double) Math.round(transferAmount * 100) / 100;
+    double transferAmount = new Random().nextDouble(MINIMUM_TRANSFER_AMOUNT, MAXIMUM_TRANSFER_AMOUNT);
+    return (double) Math.round(transferAmount * ROUNDING_FACTOR) / ROUNDING_FACTOR;
   }
 
   public static int getRandomNumberOfFiles() {
-    return new Random().nextInt(2, 6);
+    return new Random().nextInt(MINIMUM_NUMBER_OF_FILES, MAXIMUM_NUMBER_OF_FILES);
   }
 }
